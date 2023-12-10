f528cd3ba9972115b70d89847ac871e23d3ece3f
Wolferner <m.wolfern@yandex.com>
2023-12-10 03:57:23 +0200

    2-6kyu

---
+++ Is a number prime_6kyu.js
@@ -0,0 +1,24 @@
+function isPrime(num) {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isPrime(num) {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
